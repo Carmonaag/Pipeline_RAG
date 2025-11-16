e9b054fa09f1ae16d11af352917e98be09bfa3b6
Carmonaag <andregcarmona2008@gmail.com>
2025-11-16 16:02:13 -0300

    Teste_2

---
+++ main.py
@@ -0,0 +1,8 @@
+# print('Olá Mundo!!!')
+
+# define a função principal
+def main():
+    print('Executando a função principal...')
+    # chama a função principal
+    
+print('Função principal executada com sucesso!')
